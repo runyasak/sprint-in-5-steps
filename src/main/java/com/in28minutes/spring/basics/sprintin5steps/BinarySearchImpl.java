a01b25e6bb84c6432e3192c6cae8b52fc8b64a50
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-11-15 00:13:57 +0700

    feat: add BubbleSortAlgorithm and implement

---
+++ src/main/java/com/in28minutes/spring/basics/sprintin5steps/BinarySearchImpl.java
@@ -0,0 +1,10 @@
+package com.in28minutes.spring.basics.sprintin5steps;
+
+public class BinarySearchImpl {
+    public int binarySearch(int[] numbers, int numberToSearchFor) {
+        BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+        int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+
+        return 3;
+    }
+}
---
+++ src/main/java/com/in28minutes/spring/basics/sprintin5steps/BubbleSortAlgorithm.java
@@ -0,0 +1,7 @@
+package com.in28minutes.spring.basics.sprintin5steps;
+
+public class BubbleSortAlgorithm {
+    public int[] sort (int[] numbers) {
+        return numbers;
+    }
+}
