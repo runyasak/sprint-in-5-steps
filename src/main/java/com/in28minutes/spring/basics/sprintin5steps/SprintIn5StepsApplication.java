529df792132cdf5e1792fffea416550ab7875b97
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-11-14 23:43:02 +0700

    initial

---
+++ src/main/java/com/in28minutes/spring/basics/sprintin5steps/SprintIn5StepsApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spring.basics.sprintin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintIn5StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintIn5StepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/spring/basics/sprintin5steps/SprintIn5StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spring.basics.sprintin5steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintIn5StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
