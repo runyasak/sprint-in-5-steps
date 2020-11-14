382a488390a4052bdc8e11f78d09e5413171f8f9
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-11-15 00:08:48 +0700

    feat: add sort algorithms and implement

--- src/main/java/com/in28minutes/spring/basics/sprintin5steps/SprintIn5StepsApplication.java
+++ src/main/java/com/in28minutes/spring/basics/sprintin5steps/SprintIn5StepsApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SprintIn5StepsApplication {
 
 	public static void main(String[] args) {
+		BinarySearchImpl binarySearch = new BinarySearchImpl();
+		int result = binarySearch.binarySearch(new int[] {12, 4, 6}, 3);
+		System.out.println(result);
 		SpringApplication.run(SprintIn5StepsApplication.class, args);
 	}
 
