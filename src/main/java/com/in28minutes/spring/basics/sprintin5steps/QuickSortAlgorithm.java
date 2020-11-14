5d9f32f136c08606a08f34f05252609a43470a2d
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2020-11-15 00:24:42 +0700

    feat: add QuckSortAlgorithm and implement

---
+++ src/main/java/com/in28minutes/spring/basics/sprintin5steps/QuickSortAlgorithm.java
@@ -0,0 +1,7 @@
+package com.in28minutes.spring.basics.sprintin5steps;
+
+public class QuickSortAlgorithm implements SortAlgorithm {
+    public int[] sort(int[] numbers) {
+        return numbers;
+    }
+}
---
+++ src/main/java/com/in28minutes/spring/basics/sprintin5steps/SortAlgorithm.java
@@ -0,0 +1,5 @@
+package com.in28minutes.spring.basics.sprintin5steps;
+
+public interface SortAlgorightm {
+    public int[] sort (int[] numbers) {}
+}
